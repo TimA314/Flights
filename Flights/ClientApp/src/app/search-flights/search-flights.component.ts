b8c351b906eca75fc6aef5817b6b8f240218db6f
TimA314 <ci0d7j9m@mailer.me>
2022-12-03 06:37:46 -0800

    set up component and build project

---
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flights',
+  templateUrl: './search-flights.component.html',
+  styleUrls: ['./search-flights.component.css']
+})
+export class SearchFlightsComponent implements OnInit {
+  searchResult: any = [
+    "American Airlines",
+    "British Airways",
+    "United"
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
