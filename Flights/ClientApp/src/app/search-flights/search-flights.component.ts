bbb3f0dfc9c53e2829ff2ed413efe922e4f72bea
TimA314 <ci0d7j9m@mailer.me>
2022-12-03 20:16:39 -0800

    set up flight search result data and displaying

--- Flights/ClientApp/src/app/search-flights/search-flights.component.ts
+++ Flights/ClientApp/src/app/search-flights/search-flights.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-flights.component.css']
 })
 export class SearchFlightsComponent implements OnInit {
-  searchResult: any = [
-    "American Airlines",
-    "British Airways",
-    "United"
+  searchResult: FlightRm[] = [
+    {
+      airline: "American Airlines",
+      remainingNumberOfSeats: 500,
+      departure: { time: Date.now().toString(), place: "Los Angeles" },
+      arrival: { time: Date.now().toString(), place: "Instanbul" },
+      price: "350.99",
+    },
+    {
+      airline: "United",
+      remainingNumberOfSeats: 150,
+      departure: { time: Date.now().toString(), place: "Dallas" },
+      arrival: { time: Date.now().toString(), place: "Pheonix" },
+      price: "225.49",
+    },
+    {
+      airline: "JAL",
+      remainingNumberOfSeats: 600,
+      departure: { time: Date.now().toString(), place: "New York" },
+      arrival: { time: Date.now().toString(), place: "Denver" },
+      price: "1000.00",
+    }
   ]
 
   constructor() { }
@@ -17,4 +35,17 @@ export class SearchFlightsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+}
+
+export interface FlightRm {
+  airline: string;
+  arrival: TimePlaceRm;
+  departure: TimePlaceRm;
+  price: string;
+  remainingNumberOfSeats: number;
+}
+
+export interface TimePlaceRm {
+  place: string;
+  time: string;
 }
